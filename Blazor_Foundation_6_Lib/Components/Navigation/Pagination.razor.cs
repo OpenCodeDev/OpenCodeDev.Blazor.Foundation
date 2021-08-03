bd3378f8738058d10a5fcb177c7b08f22b4bcfb6
Assane DIATTA <assanediatta96@gmail.com>
2021-08-03 18:34:22 +0000

    create pagination bf6 component

--- Blazor_Foundation_6_Lib/Components/Navigation/Pagination.razor.cs
+++ Blazor_Foundation_6_Lib/Components/Navigation/Pagination.razor.cs
@@ -3,10 +3,38 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
 
 namespace OpenCodeDev.Blazor.Foundation.Components.Navigation
 {
-    class Pagination
+    public partial class Pagination : ComponentBase
     {
+        public RenderFragment ChildContent { get; set; }
+
+        /// <summary>
+        /// Custom Class to Append at the end of default Foundation Class.
+        /// </summary>
+        [Parameter]
+        public string Class { get; set; }
+
+        /// <summary>
+        /// If True, centered the pagination. (Default: False)
+        /// </summary>
+        [Parameter]
+        public bool Center { get; set; } = false;
+
+        [Parameter(CaptureUnmatchedValues = true)]
+        public IDictionary<string, object> AdditionalAttributes { get; set; }
+
+
+        public override async Task SetParametersAsync(ParameterView parameters)
+        {
+            await base.SetParametersAsync(parameters);
+        }
+
+        protected override async Task OnAfterRenderAsync(bool firstRender)
+        {
+        }
     }
 }
--- Blazor_Foundation_6_Lib/Components/Navigation/PaginationPrevious.razor.cs
+++ Blazor_Foundation_6_Lib/Components/Navigation/PaginationPrevious.razor.cs
@@ -3,10 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
 
 namespace OpenCodeDev.Blazor.Foundation.Components.Navigation
 {
-    public partial class PaginationPrevious
+    public partial class PaginationPrevious : ComponentBase
     {
+        public RenderFragment ChildContent { get; set; }
+
+        /// <summary>
+        /// Custom Class to Append at the end of default Foundation Class.
+        /// </summary>
+        [Parameter]
+        public string Class { get; set; }
+
+        /// <summary>
+        /// If True, disable previous. (Default: False)
+        /// </summary>
+        [Parameter]
+        public bool Disable { get; set; } = false;
     }
 }
