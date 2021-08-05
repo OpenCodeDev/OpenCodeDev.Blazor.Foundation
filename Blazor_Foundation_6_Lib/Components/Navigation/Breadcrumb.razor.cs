f929067cb267c2b4664d0fbf61afc8013cbe2472
Assane DIATTA <52506038+A-San96@users.noreply.github.com>
2021-08-05 14:57:01 +0000

    Add new Blazor Foundation components (#24)
    * create pagination bf6 component
    
    * create bf6 component for breadcrumbs
    
    * create bf6 component for tooltip

--- Blazor_Foundation_6_Lib/Components/Navigation/Breadcrumb.razor.cs
+++ Blazor_Foundation_6_Lib/Components/Navigation/Breadcrumb.razor.cs
@@ -3,10 +3,32 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
 
 namespace OpenCodeDev.Blazor.Foundation.Components.Navigation
 {
-    public partial class Breadcrumb
+    public partial class Breadcrumb : ComponentBase
     {
+        public RenderFragment ChildContent { get; set; }
+
+        /// <summary>
+        /// Custom Class to Append at the end of default Foundation Class.
+        /// </summary>
+        [Parameter]
+        public string Class { get; set; }
+
+        [Parameter(CaptureUnmatchedValues = true)]
+        public IDictionary<string, object> AdditionalAttributes { get; set; }
+
+
+        public override async Task SetParametersAsync(ParameterView parameters)
+        {
+            await base.SetParametersAsync(parameters);
+        }
+
+        protected override async Task OnAfterRenderAsync(bool firstRender)
+        {
+        }
     }
 }
--- Blazor_Foundation_6_Lib/Components/Navigation/PaginationItem.razor.cs
+++ Blazor_Foundation_6_Lib/Components/Navigation/PaginationItem.razor.cs
@@ -3,10 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
 
 namespace OpenCodeDev.Blazor.Foundation.Components.Navigation
 {
-    public partial class PaginationItem
+    public partial class PaginationItem : ComponentBase
     {
+        public RenderFragment ChildContent { get; set; }
+
+        /// <summary>
+        /// Custom Class to Append at the end of default Foundation Class.
+        /// </summary>
+        [Parameter]
+        public string Class { get; set; }
+
+        /// <summary>
+        /// If True, mark the current page. (Default: False)
+        /// </summary>
+        [Parameter]
+        public bool Active { get; set; } = false;
     }
 }
