86841e01af685ad91e4d737994655f5cbe0959cc
Max Samson <m.samson@flawlessloop.com>
2021-09-29 13:33:23 -0600

    Change URLs

---
+++ Blazor_Foundation_6_Test_Shared/Shared/MenuDoc.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OpenCodeDev.Blazor.Foundation.Doc.Core.Shared
+{
+    public partial class MenuDoc: ComponentBase
+    {
+         
+    }
+}
