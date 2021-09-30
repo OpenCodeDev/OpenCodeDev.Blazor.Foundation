aab568096684950dda8925dd8226b014fac21f9a
Max Samson <m.samson@flawlessloop.com>
2021-09-30 09:55:43 -0600

    Progress Bar Doc and Component Done, Reverted URL changes

--- Blazor_Foundation_6_Lib/Components/Media/ProgressBar.razor.cs
+++ Blazor_Foundation_6_Lib/Components/Media/ProgressBar.razor.cs
@@ -10,5 +10,34 @@ namespace OpenCodeDev.Blazor.Foundation.Components.Media
 {
     public partial class ProgressBar : ComponentBase
     {
+        [Parameter(CaptureUnmatchedValues = true)]
+        public IDictionary<string, object> AdditionalAttributes { get; set; }
+
+        /// <summary>
+        /// Unique HTML Identifier.
+        /// </summary>
+        [Parameter]
+        public string Id { get; set; }
+
+        /// <summary>
+        /// Custom Class to Append at the end of default Foundation Class.
+        /// </summary>
+        [Parameter]
+        public string Class { get; set; }
+
+        /// <summary>
+        /// primary (default), secondary, success, warning and alert or custom class
+        /// </summary>
+        [Parameter]
+        public string ColorClass { get; set; } = "primary";
+
+        [Parameter]
+        public float Progress { get; set; } = 0;
+
+        /// <summary>
+        /// False hide the % text inside the progress bar (default)
+        /// </summary>
+        [Parameter]
+        public bool ShowText { get; set; } = false;
     }
 }
