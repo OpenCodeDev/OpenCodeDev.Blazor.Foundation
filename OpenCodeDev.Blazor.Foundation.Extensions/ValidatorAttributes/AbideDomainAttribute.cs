ccb48d73ac9364cd7292a0b50dd08f3c028aff95
m.samson <m.samson@opencodedev.com>
2022-06-07 06:32:08 -0600

    Working on Abide

---
+++ OpenCodeDev.Blazor.Foundation.Extensions/ValidatorAttributes/AbideDomainAttribute.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Globalization;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace OpenCodeDev.Blazor.Foundation.Extensions.ValidatorAttributes
+{
+    /// <summary>
+    /// Used to define an array
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Property | AttributeTargets.Field, AllowMultiple = false)]
+    public class AbideDomainAttribute : ValidationAttribute
+    {
+        public bool AcceptsNull { get; set; } = false;
+        public AbideDomainAttribute(bool acceptsNull = false)
+        {
+            AcceptsNull = acceptsNull;
+        }
+        
+        public override bool IsValid(object value)
+        {
+            if (value == null && !AcceptsNull) { return false; }
+            var regex = new Regex(Abide.DOMAIN);
+            string valueCon = (string)value;
+            return regex.IsMatch(valueCon);
+        }
+
+        public override string FormatErrorMessage(string name)
+        {
+            return String.Format(CultureInfo.CurrentCulture, ErrorMessageString, name);
+        }
+
+    }
+}
---
+++ OpenCodeDev.Blazor.Foundation.Extensions/ValidatorAttributes/ContainInArrayAttribute.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Globalization;
+using System.Linq;
+namespace OpenCodeDev.Blazor.Foundation.Extensions.ValidatorAttributes
+{
+    /// <summary>
+    /// Used to define an array
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Property | AttributeTargets.Field, AllowMultiple = false)]
+    public class ContainsInArrayAttribute : ValidationAttribute
+    {
+        /// <summary>
+        /// List of Selectable Options
+        /// </summary>
+        public object[]? Selectable { get; set; } = null;
+
+        /// <summary>
+        /// True: will return validate true if null value is provided.
+        /// </summary>
+        public bool AcceptsNull { get; set; } = false;
+
+        public override bool IsValid(object value)
+        {
+            if (Selectable == null) { return false; }
+            if (value == null && AcceptsNull) { return true; }
+            if (value == null && !AcceptsNull) { return false; }
+            return Selectable.Contains(value);
+        }
+
+        public override string FormatErrorMessage(string name)
+        {
+            return String.Format(CultureInfo.CurrentCulture, ErrorMessageString, name);
+        }
+
+    }
+}
