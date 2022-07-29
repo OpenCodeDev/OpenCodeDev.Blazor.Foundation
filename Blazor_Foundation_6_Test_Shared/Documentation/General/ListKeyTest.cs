9ea073b70cb6ab980073ac1ef3ee073dd943b914
m.samson <m.samson@opencodedev.com>
2022-07-29 03:49:10 -0600

    Documentation List Object as Key

---
+++ Blazor_Foundation_6_Test_Shared/Documentation/General/ListKeyTest.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenCodeDev.Blazor.Foundation.Doc.Core.Documentation.General
+{
+public class ListKeyTest
+{
+    public string Name { get; set; }
+}
+}
