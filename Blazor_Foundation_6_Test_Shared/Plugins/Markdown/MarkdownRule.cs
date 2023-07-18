1a75ed545c260a4e8ba554c04b77e5a746e1bc79
m.samson <m.samson@opencodedev.com>
2023-07-17 21:56:20 -0600

    Working on Blazor Markdown Renderer

---
+++ Blazor_Foundation_6_Test_Shared/Plugins/Markdown/MarkdownRule.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace OpenCodeDev.Blazor.Foundation.Doc.Core.Plugins.Markdown
+{
+    public class MarkdownRule
+    {
+        public string Regex { get; private set; }
+        public Func<string, Match, int,MarkdownElement?>  ProcessValue { get; private set; }
+
+        /// <summary>
+        /// Mardown Rule Regex Lookup and Type of Object.
+        /// </summary>
+        /// <param name="regex"></param>
+        /// <param name="type"></param>
+        /// <param name="processValue">Process Value found by Regex, return MarkdownElement eg. # = H1, ## = H2 and int is position</param>
+        public MarkdownRule(string regex, Func<string, Match, int, MarkdownElement?> processValue)
+        {
+            Regex = regex;
+            ProcessValue = processValue;
+        }
+    }
+}
